0204676ed79eb145ee6f3820358678c128e5e0d2
Shady Ali <121682078+SHIXOOM@users.noreply.github.com>
2025-05-24 10:02:25 +0300

    Feature: Updated RandomInitializer Class to support QAP problem instances. Minor refactoring and cleaning.

--- src/PopulationInitializers/RandomInitializer.py
+++ src/PopulationInitializers/RandomInitializer.py
@@ -1,28 +1,31 @@
-import tsplib95
-from src.PopulationInitializers.PopulationInitializer import PopulationInitializer
 import random
+
+from src.PopulationInitializers.PopulationInitializer import PopulationInitializer
+from src.QAPLoader.QAPProblem import QAPProblem
+
 
 class RandomInitializer(PopulationInitializer):
     def __init__(self):
         self.problem = None
-    
-    def initialize(self, population_size: int, problem: tsplib95.models.StandardProblem) -> list[tuple[list, int]]:
-        """ returns a list of tuples. each tuple contains a 1-based tsp trace and its length """
-        self.problem = problem
 
-        node_count = problem.dimension
+    def initialize(
+        self, population_size: int, problem: QAPProblem
+    ) -> list[tuple[list, int]]:
+        """returns a list of tuples. each tuple contains a 1-based tsp trace and its length"""
+        self.problem: QAPProblem = problem
+
+        node_count = problem.n
         population = []
-        for i in range(population_size*6):
+        for i in range(population_size * 6):
             tour = [point for point in range(1, node_count + 1)]
             random.shuffle(tour)
-            traceDistance = problem.trace_tours([tour])
-            population.append((tour, traceDistance[0]))
-            
-            
-        
+            trace_fitness = problem.calculate_cost(tour)
+            population.append((tour, trace_fitness[0]))
+
         population.sort(key=lambda x: x[1], reverse=True)
-        population = population[-population_size:]
+        population = population[-population_size:]      # This is necessairy, it takes the smallest "population_size" solutions.
         return population
+
 
 # problem = tsplib95.load('/Users/shadyali/Downloads/clu_25_10.tsp')
 # ri = RandomInitializer(problem)
